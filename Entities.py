ac2f238babc0a82bc5d3758766a70f4e05e070f3
KustelR <dmitriysstudios2@gmail.com>
2026-05-03 15:13:26 +0400

    added task class

--- Entities.py
+++ Entities.py
@@ -67,4 +67,47 @@ class Chunk:
     def delta(self):
         pass
 
+class Task:
+    igrok: Igrok
+    chunk: Chunk | None
+    faction: Faction
+    duration: int
+    started = False
+    start_invoke: Callable
+    performing_invoke: Callable
+    end_invoke: Callable
+
+    def __init__(self, 
+                 igrok: Igrok, 
+                 chunk: Chunk, 
+                 faction: Faction, 
+                 duration: int,
+                 start_invoke: Callable,
+                 performing_invoke: Callable, 
+                 end_invoke: Callable):
+        self.igrok = igrok
+        self.chunk = chunk
+        self.faction = faction
+        self.duration = duration
+        self.start_invoke = start_invoke
+        self.performing_invoke = performing_invoke
+        self.end_invoke = end_invoke
+
+    def delta(self):
+        if not self.started:
+            self.started = True
+            self.start_invoke(self)
+        if self.duration > 0:
+            self.duration -=1
+            self.performing_invoke(self)
+        else:
+            self.end_invoke(self)
+
+    def to_dict(self) -> dict:
+        return {
+            "igrok": str(self.igrok.id),
+            "chunk": self.chunk.pos,
+            "faction": str(self.faction.id),
+            "duration": self.duration
+        }
 
