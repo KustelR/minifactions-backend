2ede0676cf15e88fe4f08a8c794a75f264662ac9
KustelR <dmitriysstudios2@gmail.com>
2026-05-03 10:15:07 +0400

    faction class

---
+++ Faction.py
@@ -0,0 +1,21 @@
+from Grid import Chunk
+
+
+class Faction:
+    name = None
+    igroks = []
+    chunks = []
+    emeralds = 0
+    building_materials = 0
+    food = 0
+    raw_materias = 0
+    weapons = 0
+
+    def __init__(self, name: str):
+        self.name = name
+    
+    def claim(self, chunk: Chunk):
+        chunk.owned_by = self
+
+    def delta(self):
+        pass
