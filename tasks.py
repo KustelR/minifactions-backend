8b93ccb2b6738665292440d70ffc392aaaabdba0
KustelR <dmitriysstudios2@gmail.com>
2026-05-07 13:39:47 +0400

    allowed predefined duration for stored tasks

--- tasks.py
+++ tasks.py
@@ -8,17 +8,28 @@ class StoredTask:
     start_invoke: callable[Task] | None = None
     during_invoke: callable[Task] | None = None
     end_invoke: callable[Task] | None = None
+    duration: int | None
     def __init__(self, name: str, 
                  start_invoke: callable[Task] | None = None, 
                  during_invoke: callable[Task] | None = None, 
-                 end_invoke: callable[Task] | None = None):
+                 end_invoke: callable[Task] | None = None,
+                 duration: int | None = None):
         self.name = name
         self.start_invoke = start_invoke
         self.during_invoke = during_invoke
         self.end_invoke = end_invoke
+        self.duration = duration
 
-    def task(self, igrok: Igrok, chunk: Chunk, faction: Faction, duration: int):
-        return Task(self.name, igrok, chunk, faction, duration, self.start_invoke, self.during_invoke, self.end_invoke)
+    def task(self, igrok: Igrok, chunk: Chunk, faction: Faction, duration: int | None = None):
+        return Task(
+            self.name, 
+            igrok,
+            chunk,
+            faction,
+            self.duration if self.duration else duration if duration else 1,
+            self.start_invoke,
+            self.during_invoke,
+            self.end_invoke)
 
 
 def during_foraging(task: Task):
@@ -35,6 +46,7 @@ def during_mining(task: Task):
     if task.chunk.terrain_type == "mountain":
         task.faction.raw_materials += 10
         task.faction.emeralds + 5
+
 
 foraging = StoredTask(
     "foraging",
