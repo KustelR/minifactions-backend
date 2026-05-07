d388db2226c8dd8cabadb0854940fa560252bcab
KustelR <dmitriysstudios2@gmail.com>
2026-05-07 15:02:42 +0400

    added building_farm task

--- task_manager.py
+++ task_manager.py
@@ -1,10 +1,11 @@
-from tasks import mining, foraging
+from tasks import mining, foraging, building_farm
 
 
 def get_tasks():
     result = {
         foraging.task.name: foraging.task,
-        mining.task.name: mining.task
+        mining.task.name: mining.task,
+        building_farm.task.name: building_farm.task
     }
 
     return result
---
+++ tasks/building_farm.py
@@ -0,0 +1,28 @@
+
+
+import random
+
+from Entities import Task
+from buildings.Farm import Farm
+from tasks.StoredTask import StoredTask
+
+def during_invoke(task: Task):
+    task.igrok.stamina -= 0.3
+    if (task.faction.building_materials < 2):
+        task.interrupt()
+        return
+    task.faction.building_materials -= 2
+
+def end_invoke(task: Task):
+    task.chunk.building = Farm(
+        task.chunk,
+        task.igrok.tech * 2 + 5 * random.random(),
+        task.igrok.building * 1.5 + 5 * random.random()
+    )
+
+task = StoredTask(
+    "building_farm",
+    during_invoke=during_invoke,
+    end_invoke=end_invoke,
+    duration=3
+)
